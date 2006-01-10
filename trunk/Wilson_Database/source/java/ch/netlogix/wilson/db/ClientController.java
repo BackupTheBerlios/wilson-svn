554a0c8786f1ae33b833c95d5861899cf4b58562
rdudler <rdudler@daefb20a-060a-0410-9c0d-8a0d8bd9953e>
2006-01-10 21:04:33 +0000

    DatabaseAccess Framework

---
+++ trunk/Wilson_Database/source/java/ch/netlogix/wilson/db/ClientController.java
@@ -0,0 +1,25 @@
+/**
+ * @(#) ClientController.java
+ */
+package ch.netlogix.wilson.db;
+
+import ch.netlogix.wilson.core.data.db.AbstractDatabaseController;
+
+public class ClientController extends AbstractDatabaseController {
+
+	public void startup() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void shutdown() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void notify(int event) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
